819dc1d431307ecad40a525f3c92166ed3026bc8
HARSH B CHAVAN <harshchavan404@gmail.com>
2025-11-10 20:20:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
   pingTimeout: 60000,
   pingInterval: 25000,
+  cors: {
+    origin: "*", // Allow all origins for now
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(express.static(path.join(__dirname, "public")));
